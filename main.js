a7a9d9fc1c60725538309e724376c79dc9177306
KnoWingFly <112180149+KnoWingFly@users.noreply.github.com>
2024-03-09 22:00:02 +0700

    Edit, hapus udh bisa

--- main.js
+++ main.js
@@ -1,17 +1,50 @@
 var data = [
-    {"nim": "0000000001", "nama": "admin", "alamat": "system"},
+    {"NIM" : "00000000001", "nama": "Admin", "Alamat": "Tangerang"},
 ];
 
-$(document).ready(function(){
-    var tbody = $('#bodytable');
-    
-    data.forEach(item => {
+function updatetable(data){
+    var table = document.getElementById("tablebody");
+    table.innerHTML = ""; 
+
+    for(var i=0; i < data.length; i++){
+        var item = data[i];
         var row = `<tr>
-                       <td>${item.nim}</td>
-                       <td>${item.nama}</td>
-                       <td>${item.alamat}</td>
-                       <td><button type="button" class="btn btn-primary">Ubah</button>  <button type="button" class="btn btn-danger">Hapus</button></td>
-                   </tr>`;
-        tbody.append(row);
+                    <td>${item.NIM}</td>
+                    <td>${item.nama}</td>
+                    <td>${item.Alamat}</td>
+                    <td><button type="button" class="btn btn-primary editButton" data-index="${i}">Ubah</button>  <button type="button" class="btn btn-danger deleteButton">Hapus</button></td>
+                    </tr>`;
+        table.innerHTML += row;
+    }
+}
+
+var currentIndex; 
+
+$(document).ready(function(){
+    $('body').on('click', '.editButton', function(){
+        currentIndex = $(this).data('index'); 
+        var item = data[currentIndex];
+
+        $('#NIM').val(item.NIM);
+        $('#Nama').val(item.nama);
+        $('#alamat').val(item.Alamat);
+        $('#editModal').modal('show');
     });
-})
+
+    $('.btn-primary').click(function() {
+        data[currentIndex].NIM = $('#NIM').val(); 
+        data[currentIndex].nama = $('#Nama').val();
+        data[currentIndex].Alamat = $('#alamat').val();
+        $('#editModal').modal('hide');
+        updatetable(data); 
+    });
+   
+    $('body').on('click', '.deleteButton', function(){
+        var index = $(this).closest('tr').index(); 
+        data.splice(index, 1);
+        updatetable(data); 
+    });
+});
+
+
+updatetable(data);
