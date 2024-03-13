4c16b27dfa3ad6a3aaa9f69c21bafae1e4b1367e
KnoWingFly <112180149+KnoWingFly@users.noreply.github.com>
2024-03-13 12:41:28 +0700

    logika done, design belum

--- main.js
+++ main.js
@@ -1,50 +1,83 @@
-var data = [
-    {"NIM" : "00000000001", "nama": "Admin", "Alamat": "Tangerang"},
-];
+$(document).ready(function() {
+    var data = [
+        {"NIM" : "00000000001", "nama": "Admin", "alamat": "Tangerang"},
+    ];
 
-function updatetable(data){
-    var table = document.getElementById("tablebody");
-    table.innerHTML = ""; 
+    function updatetable(data){
+        var table = $("#tablebody");
+        table.empty(); 
 
-    for(var i=0; i < data.length; i++){
-        var item = data[i];
-        var row = `<tr>
-                    <td>${item.NIM}</td>
-                    <td>${item.nama}</td>
-                    <td>${item.Alamat}</td>
-                    <td><button type="button" class="btn btn-primary editButton" data-index="${i}">Ubah</button>  <button type="button" class="btn btn-danger deleteButton">Hapus</button></td>
-                    </tr>`;
-        table.innerHTML += row;
+        for(var i=0; i < data.length; i++){
+            var item = data[i];
+            var row = `<tr>
+                        <td>${item.NIM}</td>
+                        <td>${item.nama}</td>
+                        <td>${item.alamat}</td>
+                        <td> <button type="button" class="btn btn-primary editButton" data-index="${i}">Ubah</button>  
+                             <button type="button" class="btn btn-danger deleteButton">Hapus</button>
+                        </td>
+                        </tr>`;
+            table.append(row);
+        }
     }
-}
 
-var currentIndex; 
+    $('#addButton').click(function() {
+        var NIM = $('#inputNIM').val().trim();
+        var nama = $('#inputnama').val().trim();
+        var alamat = $('#inputalamat').val().trim();
 
-$(document).ready(function(){
-    $('body').on('click', '.editButton', function(){
-        currentIndex = $(this).data('index'); 
-        var item = data[currentIndex];
+        if(!NIM || !nama || !alamat){
+            alert('Tolong isi semua form');
+            return;
+        }
 
-        $('#NIM').val(item.NIM);
-        $('#Nama').val(item.nama);
-        $('#alamat').val(item.Alamat);
-        $('#editModal').modal('show');
-    });
-
-    $('.btn-primary').click(function() {
-        data[currentIndex].NIM = $('#NIM').val(); 
-        data[currentIndex].nama = $('#Nama').val();
-        data[currentIndex].Alamat = $('#alamat').val();
-        $('#editModal').modal('hide');
+        data.push({NIM: NIM, nama: nama, alamat: alamat});
         updatetable(data); 
+
+        $('#inputNIM').val('');
+        $('#inputnama').val('');
+        $('#inputalamat').val('');
     });
-   
+
     $('body').on('click', '.deleteButton', function(){
         var index = $(this).closest('tr').index(); 
         data.splice(index, 1);
         updatetable(data); 
     });
+
+    $('.btn-warning').click(function() {
+        $('#inputNIM').val('');
+        $('#inputnama').val('');
+        $('#inputalamat').val('');
+    });
+
+    var indexToEdit; 
+
+    $('body').on('click', '.editButton', function(){
+        indexToEdit = $(this).data('index');  
+        var item = data[indexToEdit];
+
+        $('#NIM').val(item.NIM);
+        $('#nama').val(item.nama);
+        $('#alamat').val(item.alamat);
+        $('#editModal').modal('show');
+    });
+
+    $('#savedata').click(function(){
+
+        data[indexToEdit].NIM = $('#NIM').val().trim();; 
+        data[indexToEdit].nama = $('#nama').val().trim();;
+        data[indexToEdit].alamat = $('#alamat').val().trim();
+
+        if(!data[indexToEdit].NIM || !data[indexToEdit].nama || !data[indexToEdit].alamat){
+            alert('Tolong jangan kosongkan data');
+            return;
+        }
+
+        $('#editModal').modal('hide');
+        updatetable(data);
+    });
+
+
+    updatetable(data);
 });
-
-
-updatetable(data);
